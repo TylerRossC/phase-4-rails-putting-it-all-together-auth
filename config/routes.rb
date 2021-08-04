2ba12dd5cdd05fe5ae6e3362eb3bdffde5381d14
tyler <tylercharawell@gmail.com>
2021-08-04 13:57:52 -0700

    commit tree 2561454d5ff027ae9ded9a4395d36ab770759c96

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # resources :recipes
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  get '/me', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
 end
